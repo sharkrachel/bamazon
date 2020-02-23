a86b427a03c0d9f86d65e43b3a388424f6d9c985
Rachel Sipes <rasipes@gmail.com>
2020-02-23 17:57:19 -0500

    added add stock function

--- bamazonManager.js
+++ bamazonManager.js
@@ -50,11 +50,52 @@ function managerMenu() {
 function products() {
     connection.query("SELECT item_id, product_name, price, stock_quantity FROM products", function(err, res) {
         console.table(res);
+        managerMenu()
     })
 }
 
 function lowInventory() {
     connection.query("SELECT * FROM products WHERE stock_quantity < 5", function(err, res) {
         console.table(res);
+        managerMenu()
+    });
+}
+
+function itemLookUp() {
+    return new Promise(function(resolve, reject) {
+        connection.query("SELECT item_id, product_name FROM products", function(err, res){
+            resolve(res);
+        })
+    })  
+}
+
+function addInventory() {
+
+    //original - [{product_name:"dogbrush"},{product_name:"couch"}]
+    //transformed array according to return  -["5 - dogbrush"," 7 couch"]
+    itemLookUp().then(function(res) {
+        var listProducts = res.map(function(res){
+            return res.item_id + " - " + res.product_name;
+        })
+        inquirer.prompt([
+            {
+                type: "list",
+                message: "Please select a product",
+                choices: listProducts,
+                name: "productList"
+            },
+            {
+                type: "input",
+                message: "Quantity?",
+                name: "quantity"
+            }
+        ]).then(function(input) {
+               var item_id = input.productList.split(" - ")[0]
+
+               connection.query("UPDATE products SET stock_quantity = stock_quantity + ? WHERE item_id = ?", [input.quantity, item_id], function(err, res){
+                products();
+               })
+               
+        })
     })
 }
