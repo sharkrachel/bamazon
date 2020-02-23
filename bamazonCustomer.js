efdebaf07546b796bd1cfe9c2399b63ee92c10aa
Rachel Sipes <rasipes@gmail.com>
2020-02-23 15:12:53 -0500

    added process order function, but code as a sql syntax error

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,19 +9,59 @@ var connection = mysql.createConnection({
     database: "bamazonDB"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
-     // call first function here
-     showItems();
+    // call first function here
+    showItems();
 });
 
 // here is where the inquirer prompt may start...
 
 function showItems() {
-    connection.query ("SELECT * FROM products", function (err, res) {
+    connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         for (var i = 0; i < res.length; i++) {
             console.log("Item ID: " + res[i].item_id + "\n" + "Product Name: " + res[i].product_name + "\n" + "Price: " + res[i].price + "\n-------------------------");
         }
+        orderItemsPrompt();
     });
+}
+
+function orderItemsPrompt() {
+    inquirer.prompt([
+        {
+            name: "productSelection",
+            type: "input",
+            message: "Please enter the item number for the product you'd like to purchase",
+            filter:Number
+        },
+        {
+            name: "productSelectionQuantity",
+            type: "input",
+            message: "How many would you like to purchase?",
+            filter:Number
+        }
+
+    ]).then(function (answer) {
+        var requestedQuantity = answer.productSelectionQuantity;
+        var requestedItem = answer.productSelection;
+        processOrder(requestedItem, requestedQuantity);
+    });
+
+}
+
+function processOrder(id, amount) {
+    connection.query("SELECT * FROM products WHERE item_id = " + id, function(err, res) {
+        if (err) throw err;
+        if (amount <= res[0].stock_quantity) {
+            var amountOwed = res[0].price * amount;
+            console.log("Processing your oder...");
+            console.log("You owe: " + amountOwed);
+            
+            connection.query("UPDATE products SET stock_quantity = stock_quantity -" + amount + "WHERE item_id = " + id);
+        }
+        else {
+            console.log("Out of Stock");
+        }
+    })
 }
